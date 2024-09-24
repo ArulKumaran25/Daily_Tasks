0297df767beb428334423794512462a9d9c7d095
Arul Kumaran25 <arulkumaran2519@gail.com>
2024-09-24 14:08:23 +0530

    Day 3 tasks

--- Day 3/add_function.ts
+++ Day 3/add_function.ts
@@ -1,19 +1,6 @@
-<<<<<<< HEAD
 function addNumbers(a:number,b:number) // add numbers
 {
     return a+b;
 }
 var sum=addNumbers(10,15);
 console.log("Sum of a and b:"+sum);
-
-
-=======
-function addNumbers(a:number,b:number) // add numbers
-{
-    return a+b;
-}
-var sum=addNumbers(10,15);
-console.log("Sum of a and b:"+sum);
-
-
->>>>>>> ea6d1399391476752ba9663ff20955019d5c52b5
