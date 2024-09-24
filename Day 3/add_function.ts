f3c8918e4964f4caf3b6d3d536b4d3601c44c92d
Arul Kumaran25 <arulkumaran2519@gail.com>
2024-09-24 13:03:59 +0530

    Day 3 taks

---
+++ Day 3/add_function.ts
@@ -0,0 +1,8 @@
+function addNumbers(a:number,b:number) // add numbers
+{
+    return a+b;
+}
+var sum=addNumbers(10,15);
+console.log("Sum of a and b:"+sum);
+
+
---
+++ Day 3/type_annotation.js
@@ -0,0 +1,14 @@
+var age = 21;
+var Name = 'Arul Kumaran';
+var isUpdated = true;
+console.log(age, Name, isUpdated);
+function display(id, name) {
+    console.log("Id = " + id + ", Name = " + name);
+}
+display(101, "Arul Kumaran");
+var employee;
+employee = {
+    id1: 100,
+    name1: "John"
+};
+console.log(employee);
