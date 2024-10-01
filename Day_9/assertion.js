0725ead02e22178376eb62eb73245d6f6a5354c9
Arul Kumaran25 <arulkumaran2519@gail.com>
2024-10-01 17:15:56 +0530

    Day 9 tasks

--- Day_9/assertion.js
+++ Day_9/assertion.js
@@ -1,9 +1,9 @@
-var color;
+var color; //type assertion (method:1)
 color = 'red';
 var endsWithD = color.endsWith('d');
 var alternativeEndswithD = color.endsWith('a');
 console.log(endsWithD);
 console.log(alternativeEndswithD);
-var colour = 'red';
+var colour = 'red'; // type assertion (method:2) 
 var endsWith = color.endsWith('d');
 console.log(endsWith);
