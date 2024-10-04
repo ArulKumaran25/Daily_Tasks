302b537bda0b60320f84687373e8900957bd412e
Arul Kumaran25 <arulkumaran2519@gail.com>
2024-10-04 17:16:19 +0530

    Day-11 tasks

---
+++ Day_11/optional_parameter.js
@@ -0,0 +1,12 @@
+function student(name, age, isMajor) {
+    console.log(name);
+    console.log(age);
+    console.log(isMajor); // example for normal functional parameters
+}
+student("Arul", 21, true);
+function student1(name, age, isMajor) {
+    console.log(name);
+    console.log(age);
+    console.log(isMajor); // example of optional parameters
+}
+student1("Arul", 21);
