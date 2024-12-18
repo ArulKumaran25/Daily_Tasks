39ebdc09bbfb63163b4bb809754cf0234e71124f
Arul Kumaran25 <arulkumaran2519@gail.com>
2024-12-18 12:34:04 +0530

    assertion

---
+++ assertion.js
@@ -0,0 +1,9 @@
+var color; //type assertion (method:1)
+color = 'red';
+var endsWithD = color.endsWith('d');
+var alternativeEndswithD = color.endsWith('a');
+console.log(endsWithD);
+console.log(alternativeEndswithD);
+var colour = 'red'; // type assertion (method:2) 
+var endsWith = color.endsWith('d');
+console.log(endsWith);
