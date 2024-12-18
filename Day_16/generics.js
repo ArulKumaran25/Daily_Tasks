b2e6bdfd8b540e3ed532c698a23ecd752314d0a8
Arul Kumaran25 <arulkumaran2519@gail.com>
2024-12-18 12:02:17 +0530

    Files deleted

--- Day_16/generics.js
+++
@@ -1,9 +0,0 @@
-function identity(value) {
-    return value;
-}
-var num = identity(42);
-console.log(num);
-var str = identity("Hello All");
-console.log(str);
-var arr = identity([1, 2, 3, 4]);
-console.log(arr);
