87eedf794e9769abbc0bf48b417e2df2982f2549
Arul Kumaran25 <arulkumaran2519@gail.com>
2024-12-18 12:08:31 +0530

    optional and rest parameters

---
+++ rest_parameters.js
@@ -0,0 +1,8 @@
+function test() {
+    var names = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        names[_i] = arguments[_i];
+    }
+    console.log(names);
+}
+test("Arul", "Aswin", "Balaji", "Rahul");
---
+++ rest_parameters.ts
@@ -0,0 +1,7 @@
+function test(...names:string[]){
+    console.log(names);
+}
+test("Arul","Aswin","Balaji","Rahul");
+
+/* "... denotes the rest parameters
+using which it can pass many number of arguments" */
