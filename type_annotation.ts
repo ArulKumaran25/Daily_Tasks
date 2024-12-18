2870c9d48f2d29ae091bf31e0c8a641a8d87634e
Arul Kumaran25 <arulkumaran2519@gail.com>
2024-12-18 12:34:33 +0530

    type annotations

---
+++ type_annotation.ts
@@ -0,0 +1,23 @@
+var age:number=21;
+var Name:string='Arul Kumaran';
+var isUpdated:boolean=true;
+console.log(age,Name,isUpdated);
+
+
+function Display(id:number ,name:string) 
+{
+    console.log("Id = "+id+", Name = "+name);  
+}
+Display(101,"Arul Kumaran");
+
+var employee : { 
+    id1: number; 
+    name1: string; 
+}; 
+
+employee = { 
+  id1: 100, 
+  name1 : "Aswin"
+}
+console.log(employee);
+
