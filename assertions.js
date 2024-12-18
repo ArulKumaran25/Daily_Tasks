735d6a7aa5185fde3f15daff9a9f78fd19191afe
Arul Kumaran25 <arulkumaran2519@gail.com>
2024-12-18 12:06:40 +0530

    type alias and assertions

---
+++ assertions.js
@@ -0,0 +1,9 @@
+var employees = [
+    { name: 'Arul', age: 21, isMajor: true },
+    { name: 'Kumaran', age: 21, isMajor: true }
+];
+var newEmployees = employees; //assertions that insist the employee as an array type before the console 
+console.log(newEmployees[1].name);
+console.log(newEmployees[0].age);
+
+
